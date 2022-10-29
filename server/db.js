ba58a09dfb1bc93bf9d5ec3c563ac26cfe06875d
apykwan <apykwan@hotmail.com>
2022-10-29 14:01:47 -0700

    add .env

--- server/db.js
+++ server/db.js
@@ -1,8 +1,8 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "root",
+    user: process.env.PSQL_USER,
+    password: process.env.PSQL_PASSWORD,
     host: "localhost",
     port: 5432,
     database: "perntodo"
